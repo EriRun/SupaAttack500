67a989b46cbd869fc2ce6045820dad8ab640c24d
00erirun <00erirun@MS714105>
2022-10-18 18:17:33 +0200

    Fixed Player constructor

--- SupaAttack500/Player.cs
+++ SupaAttack500/Player.cs
@@ -2,8 +2,8 @@
 {
     public class Player
     {
-        public Player(string name, int level, int experiencePoints, int healthPoints, int attackDamage, int gold) { this.Name = ""; this.Level = 1; this.ExperiencePoints = 0; 
-            this.HealthPoints = 100; this.AttackDamage = 14; this.Gold = 0;
+        public Player(string name, int level, int experiencePoints, int healthPoints, int attackDamage, int gold) { this.Name = name; this.Level = level;
+            this.ExperiencePoints = experiencePoints; this.HealthPoints = healthPoints; this.AttackDamage = attackDamage; this.Gold = gold;
         }
         public string Name { get; set; }
         public int Level { get; set; }
